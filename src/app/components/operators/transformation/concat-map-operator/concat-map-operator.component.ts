53c0a1df51c014a921f33561a5dac5de902cbdf5
Virgnia <virginia@devanddel.com>
2022-02-07 19:03:56 +0100

    fix concatMap

--- src/app/components/operators/transformation/concat-map-operator/concat-map-operator.component.ts
+++ src/app/components/operators/transformation/concat-map-operator/concat-map-operator.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { concatMap, fromEvent, interval, map, take, tap } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { concatMap, fromEvent, interval, map, Subscription, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-concat-map-operator',
   templateUrl: './concat-map-operator.component.html',
   styleUrls: ['./concat-map-operator.component.scss']
 })
-export class ConcatMapOperatorComponent implements OnInit {
+export class ConcatMapOperatorComponent implements OnInit, OnDestroy {
   data: string[] = []
   initialData: string[] = []
-  counter = 0;
+  counter = 0
+  subscription!: Subscription;
 
   constructor() {}
 
@@ -20,24 +21,39 @@ export class ConcatMapOperatorComponent implements OnInit {
   onClickButton() {
     this.initialData = []
     this.data = []
+
+    if(this.subscription) {
+      this.subscription.unsubscribe();
+    }
     this.observableCreation()
   }
 
   observableCreation() {
-
-    const clicks = fromEvent(document, 'click').pipe(map(() => {
-      this.counter++;
-      this.initialData = [...this.initialData, 'click ' + this.counter];
-      return this.counter;
-    }));
-
-    clicks.pipe(concatMap(counter =>
-      interval(1000).pipe(
-      take(3),
-      tap((data) => {
-        this.data = [...this.data, `click ${counter} - ${data}` ];
+    const clicks = fromEvent(document, 'click').pipe(
+      map(() => {
+        this.counter++
+        this.initialData = [...this.initialData, 'click ' + this.counter]
+        return this.counter
       })
-    ))).subscribe();
-}
+    )
 
+   this.subscription = clicks
+      .pipe(
+        concatMap((counter) =>
+          interval(1000).pipe(
+            take(3),
+            tap((data) => {
+              this.data = [...this.data, `click ${counter} - ${data}`]
+            })
+          )
+        )
+      )
+      .subscribe()
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+    }
+  }
 }
